38d8eafe553c63a6f8a2089ee8df892bcc636b8d
平林 <zhy.ai@qq.com>
2016-12-29 15:44:58 +0800

    提交项目

--- app/src/main/java/com/example/intnet1/PhotoIntentActivity.java
+++ app/src/main/java/com/example/intnet1/PhotoIntentActivity.java
@@ -1,8 +1,12 @@
 package com.example.intnet1;
 
+import android.app.Activity;
+
 /**
  * Created by pingL on 2016/12/29,下午2:56.
  */
 
-public class PhotoIntentActivity extends  {
+public class PhotoIntentActivity extends Activity {
+
+
 }
